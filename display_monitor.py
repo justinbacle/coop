0511a19440caf0ed3c740aa18ae68d692799a9ce
Justin Bacle <justin.bacle@gmail.com>
2020-01-13 19:44:30 +0100

    packed everythin into a nice class

--- display_monitor.py
+++ display_monitor.py
@@ -4,34 +4,45 @@ import time
 
 class TuiMonitor():
 
-    # Handle key presses
-    def _handleInput(key):
-        if key == 'Q' or key == 'q':
-            raise urwid.ExitMainLoop()
+    items = []
 
     def __init__(self):
-        header_text = urwid.Text(u'Test Program')
-        header = urwid.AttrMap(header_text, 'titlebar')
-
-        self.bottomText = urwid.Text([
-            u'Press (', ('quit button', u'Q'), u') to quit.'
-        ])
-
         self.quote_text = urwid.Text(u'Press (R) to get your first quote!')
         quote_filler = urwid.Filler(self.quote_text, valign='top', top=1, bottom=1)
         v_padding = urwid.Padding(quote_filler, left=1, right=1)
-        self.quote_box = urwid.LineBox(v_padding)
+        self.mainBody = urwid.LineBox(v_padding)
+
+        self.setGLobalLayout()
 
         # Set up color scheme
         palette = []
 
-        layout = urwid.Frame(header=header, body=self.quote_box, footer=self.bottomText)
-        self.main_loop = urwid.MainLoop(layout, palette, unhandled_input=self._handleInput)
+        self.main_loop = urwid.MainLoop(self.mainFrame, palette, unhandled_input=self._handleInput)
+
+    # Handle key presses
+    def _handleInput(self, key):
+        if key == 'Q' or key == 'q':
+            raise urwid.ExitMainLoop()
+
+    def setGLobalLayout(self):
+        # ------------ HEADER ------------
+        header_text = urwid.Text(u'Test Program')
+        self.header = urwid.AttrMap(header_text, 'titlebar')
+        self.items.append(self.header)
+
+        # ----------- FOOTER -------------
+        self.footer = urwid.Text([
+            u'Press (', ('quit button', u'Q'), u') to quit.'
+        ])
+        self.items.append(self.footer)
+
+        # ----------- MAIN LAYOUT -------------
+        self.mainFrame = urwid.Frame(header=self.header, body=self.mainBody, footer=self.footer)
 
     def _refresh(self, _loop, _data):
         self.main_loop.draw_screen()
         timeText = u''.join(str(time.ctime()))
-        self.quote_box.base_widget.set_text(timeText)
+        self.footer.set_text(timeText)
         self.main_loop.set_alarm_in(1, self._refresh)
 
     def run(self):
